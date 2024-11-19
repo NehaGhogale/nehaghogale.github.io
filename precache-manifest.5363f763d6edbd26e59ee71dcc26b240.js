fbf4706dbe4c558dd86cd3612109e31859f28e42
nehaghogale <ghogale.neha@gmail.com>
2024-11-19 14:39:49 +0530

    Updates

---
+++ precache-manifest.5363f763d6edbd26e59ee71dcc26b240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e826ba60268f0c1dbd8c204c27d798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f5b7b9f96bceb2fc634",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f3e6bb58bfa7d0f80a83",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "1f5b7b9f96bceb2fc634",
+    "url": "/static/js/2.ba8a9c01.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.ba8a9c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e6bb58bfa7d0f80a83",
+    "url": "/static/js/main.9b7050e7.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
